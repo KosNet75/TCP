563f09fcc2887515daed474c6a8b6fec11ed1207
KosNet75 <nemokonstantin@gmail.com>
2022-09-30 20:15:42 +0700

    first

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,37 +3,35 @@ import java.net.Socket;
 
 public class Client {
 
-    static Socket clientSocket; //сокет для общения
-    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
-    // мы узнаем что хочет сказать клиент?
-    private static BufferedReader in; // поток чтения из сокета
-    private static BufferedWriter out; // поток записи в сокет
+    static Socket clientSocket;
+    private static BufferedReader reader;
+    private static BufferedReader in;
+    private static BufferedWriter out;
 
     public static void main(String[] args) {
-        try {
-            clientSocket = new Socket("localhost", 8989); // этой строкой мы запрашиваем
+        while (true) {
+            try {
+                clientSocket = new Socket("localhost", 8080);
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            // читать соообщения с сервера
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            // писать туда же
-            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                String serverDialogue = in.readLine();
+                System.out.println(serverDialogue);
 
-            String serverWord = in.readLine(); // ждём, что скажет сервер
-            System.out.println(serverWord); // получив - выводим на экран
+                String city = reader.readLine();
+                out.write(city + "\n");
+                out.flush();
 
-            String word = reader.readLine(); // ждём ввод
-            out.write(word + "\n"); // отправляем сообщение на сервер
-            out.flush();
-
-            String serverWord2 = in.readLine(); // ждём, что скажет сервер
-            System.out.println(serverWord2); // получив - выводим на экран
+                String dialAnswer = in.readLine();
+                System.out.println(dialAnswer);
 
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
-
 
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,101 +10,48 @@ public class Server {
     static String textTemp;
     static String text;
 
-    private static Socket clientSocket; //сокет для общения
-    private static ServerSocket server; // серверсокет
-    private static BufferedReader in; // поток чтения из сокета
-    private static BufferedWriter out; // поток записи в сокет
+    private static Socket clientSocket;
+    private static ServerSocket server;
+    private static BufferedReader in;
+    private static BufferedWriter out;
     private static char endChar;
-    private static char oneChar;
 
 
     public static void main(String[] args) {
-        /*try {
-            ServerSocket server = new ServerSocket(4004); // серверсокет прослушивает порт 4004
+
+        try (ServerSocket serverSocket = new ServerSocket(8080);) {
             System.out.println("Сервер запущен!");
             while (true) {
-
-                try {
-                    clientSocket = server.accept();
-                    System.out.println("New client connected to server");
-
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-
-
-
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream());) {
+                    char backChar = 0;
                     if (text == null) {
-                        out.write("Привет, это Сервер! Ты первый вводи любой город" + "\n");
-                        }else if (oneChar==text.charAt(text.length()-1) && text != null ){
-                        out.write("Вы ввели неправильное значение, было нужно : " + oneChar + "\n");
-                    }else {
-                        out.write("Вам на: " + endChar + "\n");
-                        oneChar=endChar;
-                    }
-
-                    out.flush();
-
-                    text = in.readLine();
-                    oneChar = text.charAt(0);
-
-
-                    System.out.println("Вам написали текст: " + text + "\n" + "Номер порта: " + clientSocket.getPort());
-                    System.out.println(endChar);
-                    System.out.println(oneChar);
-                    clientSocket.close();
-                    // потоки тоже хорошо бы закрыть
-                    in.close();
-                    out.close();
-
-
-                    // выталкиваем все из буфера
-                } finally { // в любом случае сокет будет закрыт
-                    clientSocket.close();
-                    // потоки тоже хорошо бы закрыть
-                    in.close();
-                    out.close();
-                }
-            }
-
-
-        } catch (IOException e) {
-            System.err.println(e);
-        }*/
-        try (ServerSocket serverSocket = new ServerSocket(8989);) {// стартуем сервер один(!) раз
-            System.out.println("Сервер запущен!");
-            while (true) { // в цикле(!) принимаем подключения
-                try (
-                        Socket socket = serverSocket.accept();
-                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream());
-                ) {
-                    if (text==null){
-                        out.write("???:"+"\n");
+                        out.write("Введите название города:" + "\n");
                         out.flush();
-                    }else {
-                        endChar=text.charAt(text.length()-1);
-                        out.write("Вам на "+endChar+"\n");
+                    } else {
+                        char Char = text.charAt(text.length() - 1);
+                        backChar = Character.toUpperCase(Char);
+                        out.write("Город " + text + " напишите город на букву " + backChar + "\n");
                         out.flush();
                     }
                     textTemp = in.readLine();
                     System.out.println(textTemp);
-                    if (text!=null && endChar==textTemp.charAt(0) ){
-                        out.write("OK"+"\n");
-                        text=textTemp;
-                    }else if (text==null){
-                        out.write("OK"+"\n");
-                        text=textTemp;
-                    }else{
-                        out.write("NOT OK"+"\n");
+                    if (text != null && backChar == textTemp.charAt(0) ||
+                        text != null && backChar == Character.toUpperCase(textTemp.charAt(0))) {
+                        out.write("Ответ принят." + "\n");
+                        text = textTemp;
+                    } else if (text == null) {
+                        out.write("Игра началась." + "\n");
+                        text = textTemp;
+                    } else {
+                        out.write("Не правильный ответ." + "\n");
                     }
                     out.flush();
-
-
                 }
             }
         } catch (IOException e) {
-            System.out.println("Не могу стартовать сервер");
+            System.out.println("Ошибка запуска сервера");
             e.printStackTrace();
         }
     }
