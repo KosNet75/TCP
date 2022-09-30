cf4631d03edb6b04125018475f047dfe297d32f5
KosNet75 <nemokonstantin@gmail.com>
2022-09-30 20:24:26 +0700

    first

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,35 +3,34 @@ import java.net.Socket;
 
 public class Client {
 
-    static Socket clientSocket;
-    private static BufferedReader reader;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+  static Socket clientSocket;
 
-    public static void main(String[] args) {
-        while (true) {
-            try {
-                clientSocket = new Socket("localhost", 8080);
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+  public static void main(String[] args) {
+    while (true) {
+      try {
+        clientSocket = new Socket("localhost", 8080);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
+        BufferedWriter out = new BufferedWriter(
+            new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                String serverDialogue = in.readLine();
-                System.out.println(serverDialogue);
+        String serverDialogue = in.readLine();
+        System.out.println(serverDialogue);
 
-                String city = reader.readLine();
-                out.write(city + "\n");
-                out.flush();
+        String city = reader.readLine();
+        out.write(city + "\n");
+        out.flush();
 
-                String dialAnswer = in.readLine();
-                System.out.println(dialAnswer);
+        String dialAnswer = in.readLine();
+        System.out.println(dialAnswer);
 
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
 
     }
+
+  }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,54 +7,47 @@ import java.net.Socket;
 public class Server {
 
 
-    static String textTemp;
-    static String text;
+  static String textTemp;
+  static String text;
 
-    private static Socket clientSocket;
-    private static ServerSocket server;
-    private static BufferedReader in;
-    private static BufferedWriter out;
-    private static char endChar;
+  public static void main(String[] args) {
 
-
-    public static void main(String[] args) {
-
-        try (ServerSocket serverSocket = new ServerSocket(8080);) {
-            System.out.println("Сервер запущен!");
-            while (true) {
-                try (Socket socket = serverSocket.accept();
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                     PrintWriter out = new PrintWriter(socket.getOutputStream());) {
-                    char backChar = 0;
-                    if (text == null) {
-                        out.write("Введите название города:" + "\n");
-                        out.flush();
-                    } else {
-                        char Char = text.charAt(text.length() - 1);
-                        backChar = Character.toUpperCase(Char);
-                        out.write("Город " + text + " напишите город на букву " + backChar + "\n");
-                        out.flush();
-                    }
-                    textTemp = in.readLine();
-                    System.out.println(textTemp);
-                    if (text != null && backChar == textTemp.charAt(0) ||
-                        text != null && backChar == Character.toUpperCase(textTemp.charAt(0))) {
-                        out.write("Ответ принят." + "\n");
-                        text = textTemp;
-                    } else if (text == null) {
-                        out.write("Игра началась." + "\n");
-                        text = textTemp;
-                    } else {
-                        out.write("Не правильный ответ." + "\n");
-                    }
-                    out.flush();
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка запуска сервера");
-            e.printStackTrace();
+    try (ServerSocket serverSocket = new ServerSocket(8080);) {
+      System.out.println("Сервер запущен!");
+      while (true) {
+        try (Socket socket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());) {
+          char backChar = 0;
+          if (text == null) {
+            out.write("Введите название города:" + "\n");
+            out.flush();
+          } else {
+            char Char = text.charAt(text.length() - 1);
+            backChar = Character.toUpperCase(Char);
+            out.write("Город " + text + ". Напишите город на букву '" + backChar + "'\n");
+            out.flush();
+          }
+          textTemp = in.readLine();
+          System.out.println(textTemp);
+          if (text != null && backChar == textTemp.charAt(0) ||
+              text != null && backChar == Character.toUpperCase(textTemp.charAt(0))) {
+            out.write("Ответ принят." + "\n");
+            text = textTemp;
+          } else if (text == null) {
+            out.write("Игра началась." + "\n");
+            text = textTemp;
+          } else {
+            out.write("Не правильный ответ." + "\n");
+          }
+          out.flush();
         }
+      }
+    } catch (IOException e) {
+      System.out.println("Ошибка запуска сервера");
+      e.printStackTrace();
     }
+  }
 }
 
 
